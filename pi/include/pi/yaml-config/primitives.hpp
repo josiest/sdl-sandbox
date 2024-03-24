4880d61becd2b21a43ca7bffab1914cc451ab2c5
Josie Thompson <dot.slash.josie@gmail.com>
2024-03-23 21:06:53 -0700

    moved config/primitives into yaml-config module

---
+++ pi/include/pi/yaml-config/primitives.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "pi/yaml-config/errors.hpp"
+
+#include <SDL2/SDL_rect.h>
+
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/node/convert.h>
+
+template<>
+struct YAML::convert<SDL_Point> {
+    inline static YAML::Node encode(const SDL_Point& point);
+    inline static bool decode(const YAML::Node& node, SDL_Point& point);
+};
+
+YAML::Node YAML::convert<SDL_Point>::encode(const SDL_Point& point)
+{
+    YAML::Node node;
+    node.push_back(point.x);
+    node.push_back(point.y);
+    return node;
+}
+
+static bool decode(const YAML::Node& node, SDL_Point& point)
+{
+    namespace msg = YAML::ErrorMsg;
+
+    if (not node) { return false; }
+    if (not node.IsSequence()) {
+        msg::error(node, msg::not_a_sequence);
+        return false;
+    }
+    if (node.size() != 2) {
+        msg::error(node, msg::expecting_size(2));
+        return false;
+    }
+    using as_int = YAML::convert<int>;
+    const bool has_x = as_int::decode(node[0], point.x);
+    const bool has_y = as_int::decode(node[1], point.y);
+    if (not has_x or not has_y) {
+        msg::error(node, msg::not_int_sequence);
+        return false;
+    }
+    return true;
+}
